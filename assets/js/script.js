3e469cb5e5e2f646ce48bf4d827fc3ee7fbeaeec
Valentina Segura Vargas <vale.eseve@gmail.com>
2025-06-30 01:54:46 -0400

    Ícono dark mode on/off

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  let darkMode = false;
 
   // DARK MODE TOGGLE
-  $("#control-panel__btn-dark").click(function() {
+  $("#control-panel__btn-dark").click(function () {
+    darkMode = !darkMode;
     $("body").toggleClass("dark-mode");
+    $("#control-panel__btn-i").removeClass();
+
+    if (darkMode) {
+      $("#control-panel__btn-i").addClass("fa-solid fa-sun-bright");
+    } else {
+      $("#control-panel__btn-i").addClass("fa-solid fa-moon-stars");
+    }
   });
 
-
-
-//   LANGUAGE TOGGLE
- $("#control-panel__btn-en").click(function () {
+  //   LANGUAGE TOGGLE
+  $("#control-panel__btn-en").click(function () {
     $(".i18n").each(function () {
       const texto = $(this).data("en");
       if (texto) {
@@ -25,7 +32,4 @@ $(document).ready(function() {
       }
     });
   });
-
-
-
 });
